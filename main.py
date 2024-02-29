50496f209da069135d5bf1a1142b11f0ec390372
zEuS0390 <zeusjamesbaltazar@gmail.com>
2024-03-01 02:43:15 +0800

    update locators and implement MQTT communication

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
+import paho.mqtt.client as mqtt
 from constants import *
 import time
+
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+client.username_pw_set('user', 'pass123')
+client.connect(host='localhost', port=1883)
+client.loop_start()
 
 options = Options()
 options.add_argument("-headless")
@@ -23,10 +30,15 @@ login_button.click()
 
 time.sleep(2)
 driver.get(GROUP_CHAT_LINK)
-time.sleep(10)
+time.sleep(5)
 
-old_message = ""
+old_message = {
+  "sender": "",
+  "date_and_time": "",
+  "value": ""
+}
 while True:
+
   locator = (
     By.XPATH,
     "//span[contains(@class, 'html-span xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x1hl2dhg x16tdsg8 x1vvkbs')]"
@@ -35,7 +47,9 @@ while True:
     div_elements = WebDriverWait(driver, 3).until(
       expected_conditions.presence_of_all_elements_located(locator)
     )
-  except TimeoutException: continue
+  except TimeoutException: 
+    print("Sender text missing")
+    continue
   recent_sender = div_elements[-1].text
 
   locator = (
@@ -47,14 +61,45 @@ while True:
     div_elements = WebDriverWait(driver, 3).until(
       expected_conditions.presence_of_all_elements_located(locator)
     )
-  except TimeoutException: continue
-  recent_message = div_elements[-1].text
-  if old_message != recent_message:
-    old_message = recent_message
-    print(f"{recent_sender}: {recent_message}")
+  except TimeoutException: 
+    print("Recent message missing")
+    continue
+
+  hover = ActionChains(driver).move_to_element(div_elements[-1])
+  hover.perform()
+
+  locator = (
+    By.XPATH,
+    "//div[contains(@class, 'x1cy8zhl x78zum5 xdt5ytf x193iq5w x1n2onr6 x1kxipp6')]/span[@aria-describedby]"
+  )
+  
+  date_and_time = ""
+  try:
+    message_container = WebDriverWait(driver, 3).until(
+      expected_conditions.presence_of_all_elements_located(locator)
+    )
+    tooltip_id = message_container[-1].get_attribute('aria-describedby')
+    date_and_time_tooltip = driver.find_element(By.XPATH, f"//div[@id='{tooltip_id}']/div/div/span")
+    date_and_time = date_and_time_tooltip.text
+  except TimeoutException:
+    print("Message container missing")
+
+  message = {
+    "sender": recent_sender,
+    "date_and_time": date_and_time,
+    "value": div_elements[-1].text
+  }
+
+  if old_message != message:
+    old_message = message
+    client.publish('inTopic', '1')
+    print(f"{recent_sender} [{date_and_time}]: {message["value"]}")
    
-  if recent_message.lower().strip() == "exit":
+  if message["value"].lower().strip() == "exit":
     break
 
 input("Press enter to quit...")
 driver.quit()
+
+client.loop_stop()
+client.disconnect()
